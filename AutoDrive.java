8a0e6cc4159ec37da88b0750b3fbe49a8107686d
9764Ryba <160544589+9764Ryba@users.noreply.github.com>
2024-05-15 14:15:34 -0500

    Update AutoDrive.java

--- AutoDrive.java
+++ AutoDrive.java
@@ -35,7 +35,7 @@ public class AutoDrive extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    driveSubsystem.arcadeDrive(0, 0);
+    driveSubsystem.driveOff();
     autoTimer.reset();
   }
 
