5e143fc971cdd34fa1ebe4435dfd4e50822d38cd
9764Ryba <160544589+9764Ryba@users.noreply.github.com>
2024-05-15 14:15:52 -0500

    Create AutoShoot.java

---
+++ AutoShoot.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class AutoShoot extends Command {
+  
+  ShooterSubsystem shooterSubsystem;
+  Timer shootTimer = new Timer();
+
+  public AutoShoot(ShooterSubsystem shooterSubsystem) {
+    addRequirements(shooterSubsystem);
+    this.shooterSubsystem = shooterSubsystem;
+  }
+
+
+  @Override
+  public void initialize() {
+    shootTimer.restart();
+    shooterSubsystem.prepShot();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (shootTimer.hasElapsed(1)) {
+      shooterSubsystem.shoot();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooterSubsystem.shootOff();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (shootTimer.hasElapsed(2)) {
+      return true;
+    }
+    return false;
+  }
+}
